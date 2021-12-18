b38fde9eed553c243a80eda16e97fd211100b314
Valerii Boiko <valerii.boiko01@gmail.com>
2021-12-18 22:59:02 +0200

    adds the solution fot the second task

--- Homeworks/HW_#1_Closures/app.js
+++ Homeworks/HW_#1_Closures/app.js
@@ -1,19 +1,35 @@
-function runningAverage(){
-    let num = 0;
-    function getAverage(secondNum){
-        if (num === 0){
-            num = secondNum;
-            return num;
-        }else {
-            return (num + secondNum) / 2;
-        }
+//Task#1
+function runningAverage() {
+    let currentSum = 0;
+
+    function getAverage(num) {
+        if (currentSum === 0) {
+            currentSum = num;
+            return currentSum;
+        } else return (currentSum + num) / 2;
     }
     return getAverage
 }
 
 let rAvg = runningAverage()
+console.log(rAvg(100))
+console.log(rAvg(300))
 
-console.log(rAvg(10))
-console.log(rAvg(11))
-console.log(rAvg(12))
-console.log(rAvg(12))
+
+//Task#2
+function sum(...arr){
+    let currentSum = arr.reduce((total, int) => {
+        return total + int
+    }, 0);
+    function add (num) {
+        currentSum+=num;
+        return add;
+    }
+    add.toString = function (){
+        return currentSum;
+    }
+    return add;
+}
+
+alert(sum(1)(2)(3)(100))
+alert(sum(50,50)(2)(3)(2))
